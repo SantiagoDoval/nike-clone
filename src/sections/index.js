61ae8a6086e84305d946e6ce3975cb63e3597fd4
SantiagoDoval <danieldovals@gmail.com>
2023-08-23 21:57:29 -0500

    Seccion:Navbar

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+
+export {
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    Hero,
+    PopularProducts
+}
