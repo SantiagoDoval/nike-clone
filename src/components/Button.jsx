f9abaa479aa7cbe2451378ef539d538378b969b2
SantiagoDoval <danieldovals@gmail.com>
2023-08-24 10:00:55 -0500

    Section: Special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
-const Button = ({label,iconURL}) => {
+const Button = ({label,iconURL, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-monserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-monserrat text-lg leading-none rounded-full ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`:"bg-coral-red text-white  border-coral-red "} `}>
        {label}
        {iconURL && <img className="ml-2 rounded-full w-5 h-5" src={iconURL} alt="Arrow Right Icon" />}
     </button>
